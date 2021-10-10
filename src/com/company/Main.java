a0e340d74d88a986027021db58f2919aa9dc208d
Roman <roma.savitskiy@inbox.ru>
2021-10-10 18:31:42 +0300

    line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Крестики - нолики");
+	    System.out.print("line");
+        System.out.print("Крестики - нолики");
     }
 }
